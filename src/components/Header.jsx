4b8fc8871692740c56afcb4508014ef798896861
tomi <dev.dreissiger@gmail.com>
2021-11-29 16:50:28 +0100

    header work 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,19 @@ export default function Header() {
 
     const [isSmallScreen, setIsSmallScreen] = useState(mediaQuery.matches);
     const [isVerticalMenuVisible, setIsVerticalMenuVisible] = useState(false);
-
-    /* this block is just a trial, yes */
-    const decorColors = [
-        'red', 'hotpink', 'deeppink', 'magenta', 'tomato', 
-        'springgreen', 'lightseagreen', 'darkturquoise', 'darkcyan', 'teal', 'cornflowerblue', 'royalblue',
-        'mediumvioletred', 'darkviolet'
-    ];
-    const [decorColor, setDecorColor] = useState(0);
+    const [isShadowVisible, setIsShadowVisible] = useState(false);
 
     useEffect(() => {
-        document.documentElement.style.setProperty('--red', decorColors[decorColor]);
-        return () => { };
-    }, [decorColor]);
+        function onWindowScroll() {
+            setIsShadowVisible(window.scrollY >= 64);
+        }
 
-    const onLogoClick = () => {
-        setDecorColor((dC) => dC === decorColors.length - 1 ? 0 : dC + 1);
-    };
-    /* this block is just a trial, yes */
+        window.addEventListener('scroll', onWindowScroll)
+
+        return () => {
+            window.removeEventListener('scroll', onWindowScroll);
+        };
+    }, []);
 
     useEffect(() => {
         const onMediaChange = (e) => {
@@ -39,20 +34,17 @@ export default function Header() {
 
     const Links = () => (
         <>
-            <Link href="#home">Home</Link>
-            <Link href="#about">About</Link>
-            <Link href="#features">Features</Link>
-            <Link href="#pricing">Pricing</Link>
-            <Link href="#trainers">Trainers</Link>
-            <Link href="#blogs">Blogs</Link>
-            <Link href="#colors" onClick={() => onLogoClick()}>Colors</Link>
+            <Link href="#home">Főoldal</Link>
+            <Link href="#pricing">Áraink</Link>
+            <Link href="#comments">Vélemények</Link>
+            <Link href="#footer">Kapcsolat</Link>
         </>
     );
 
     return (
-        <Container data-component="header">
-            <Logo href="#">
-                <span>Be</span>Fit
+        <Container data-component="header" isShadowVisible={isShadowVisible}>
+            <Logo href="#home">
+                <span>Bakonyi</span>Apartmanház
             </Logo>
 
             {isSmallScreen && <VerticalMenu isVerticalMenuVisible={isVerticalMenuVisible} onClick={() => setIsVerticalMenuVisible(false)}>
@@ -71,34 +63,40 @@ export default function Header() {
 const Container = styled.header`
     position: fixed;
     top: 0;
-    width: 100%;
+    left: 0;
+    right: 0;
+    height: 4rem;
     z-index: 10;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    background-color: var(--black);
-    padding: 1rem 2rem;
+    background-color: var(--day);
+    box-shadow: ${({isShadowVisible}) => isShadowVisible ? '0 0 1rem var(--light-night)' : ''};
+    padding: 0 2rem;
 
     @media(min-width: 1024px) {
-        padding: 1rem 4rem;
+        padding: 0 4rem;
     }
 
     ion-icon {
-        color: var(--white);
+        color: var(--night);
         font-size: 2.5rem;
         cursor: pointer;
     }
 `;
 
 const Logo = styled.a`
-    font-weight: 600;
-    font-size: 2rem;
-    color: var(--white);
+    font-size: 1.25rem;
+    color: var(--night);
     text-decoration: none;
     letter-spacing: 0.1rem;
 
+    @media(min-width: 768px) {
+        font-size: 1.5rem;
+    }
+
     span {
-        color: var(--red);
+        color: var(--decor);
     }
 `;
 
@@ -107,7 +105,8 @@ const VerticalMenu = styled.nav`
     top: 0;
     left: 0;
     height: 100vh;
-    background-color: var(--black);
+    background-color: var(--day);
+    box-shadow: 0 0 0.125rem var(--light-night);
     padding: 2rem 0 0 2rem;
     display: flex;
     flex-direction: column;
@@ -115,18 +114,16 @@ const VerticalMenu = styled.nav`
     transform: translate3d(${({ isVerticalMenuVisible }) => isVerticalMenuVisible ? '0' : '-100%'}, 0, 0);
 `;
 
-const HorizontalMenu = styled.nav`
-
-`;
+const HorizontalMenu = styled.nav``;
 
 const Link = styled.a`
-    color: var(--white);
+    color: var(--night);
     margin-bottom: 1rem;
     margin-right: 1rem;
     font-size: 1.5rem;
     text-decoration: none;
 
     :hover {
-        color: var(--red);
+        color: var(--decor);
     }
 `;
