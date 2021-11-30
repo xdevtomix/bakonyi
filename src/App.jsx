11c959da88b018925ebbc5bfd7f27d2184c5ddbb
tomi <dev.dreissiger@gmail.com>
2021-11-30 23:20:04 +0100

    react lazy test 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
+import React, { Suspense } from 'react';
 import styled, { StyleSheetManager } from "styled-components";
 
-import Header from "./components/Header";
+// import Header from "./components/Header";
+const Header = React.lazy(() => import('./components/Header'));
 import Home from "./components/Home";
-import Pricing from "./components/Pricing";
-import Breakfast from "./components/Breakfast";
-import Testimonials from "./components/Testimonials";
-import Footer from "./components/Footer";
+// import Pricing from "./components/Pricing";
+const Pricing = React.lazy(() => import('./components/Pricing'));
+// import Breakfast from "./components/Breakfast";
+const Breakfast = React.lazy(() => import('./components/Breakfast'));
+// import Testimonials from "./components/Testimonials";
+const Testimonials = React.lazy(() => import('./components/Testimonials'));
+// import Footer from "./components/Footer";
+const Footer = React.lazy(() => import('./components/Footer'));
 
 export default function App() {
   return (
     <StyleSheetManager disableVendorPrefixes>
       <Container data-component="app">
-        <Header />
+        <Suspense fallback={null}>
+          <Header />
+        </Suspense>
         <Home />
-        <Pricing />
-        <Breakfast />
-        <Testimonials />
-        <Footer />
+        <Suspense fallback={null}>
+          <Pricing />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Breakfast />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Testimonials />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Footer />
+        </Suspense>
       </Container>
     </StyleSheetManager>
   )
