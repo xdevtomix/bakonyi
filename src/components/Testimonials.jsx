94703609f1bdafd4c5a2f4c4b0a13226d53269d2
tomi <dev.dreissiger@gmail.com>
2021-12-01 22:31:31 +0100

    testimonials work 5

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,78 +5,89 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+const testimonials = [
+    {
+        id: 1,
+        text: `
+            Szép, tiszta és kényelmes szobát kaptunk modern berendezéssel, szép fürdőszobával. Közel van a bolt is, a buszmegálló is, a természet is.
+            A szállás éttermében vacsoráztunk, az ételek nagyon finomak voltak, jóllaktunk. Köszönjük a vendéglátást!
+        `,
+        name: 'Gergely'
+    },
+    {
+        id: 2,
+        text: `
+            Nagyon családias, igazán vendéglátásra termett tulajdonosokkal találkozhattunk. Megtapasztalhattuk az igazi vendégszeretetet.
+        `,
+        name: 'Ferenc'
+    },
+    {
+        id: 3,
+        text: `
+            Kedvesek a vendéglátók, segítőkészek. Nagyon tiszta és igazán otthonos, remekül felszerelt szálláshely.
+            Teljesen alkalmas az önellátásra, de ha akartam a vendéglő részüket is igénybe vehettem.
+            Élelmiszerbolt a közvetlen szomszédságban. Jövőre is ide mennék!
+        `,
+        name: 'Márta'
+    },
+    {
+        id: 4,
+        text: `
+            Mindenki nagyon kedves volt, segítőkész, látszott, hogy nekik fontos volt, hogy mi öten jól érezzük magunkat!
+            A szállás tiszta volt, hűvös, csendes és nyugodt! Mindenben nagyon megfelelt, jól éreztük ott magunkat!
+            Köszönjük szépen!
+        `,
+        name: 'Lívia'
+    },
+];
+
 export default function Testimonials() {
     return (
         <Container data-component="testimonials" id="velemenyek">
 
-            <Header>
-                <p>Vélemények</p>
-                <h2>Vendégeink írták</h2>
-                <div>
-                    Számunkra nagyon fontos vendégeink elégedettsége, ezért mindig örömmel tölt el,
-                    amikor elutazásuk után pozitív visszajelzést kapunk.
-                </div>
-                <button>Még több vélemény</button>
-            </Header>
+            <Title>
+                <div></div>
+                <h2>Vélemények</h2>
+                <div></div>
+            </Title>
+
+            <Info>
+                Számunkra nagyon fontos vendégeink elégedettsége, ezért mindig örömmel tölt el,
+                amikor elutazásuk után pozitív visszajelzést kapunk:
+            </Info>
 
             <SwiperContainer>
                 <Swiper
                     modules={[Pagination, Autoplay]}
                     pagination={{ clickable: true }}
-                    autoplay={{ delay: 5000 }}
+                    autoplay={{ delay: 10 * 1000 }}
                     loop
                     breakpoints={{
-                        1024: {
+                        768 : {
                             slidesPerView: 2,
+                            spaceBetween: 20
+                        },
+                        1024: {
+                            slidesPerView: 4,
                             spaceBetween: 20
                         }
                     }}
                 >
-                    <SwiperSlide>
-                        <SlideItem>
-                            <div>
-                                Szép, tiszta és kényelmes szobát kaptunk modern berendezéssel, szép fürdőszobával. Közel van a bolt is, a buszmegálló is, a természet is.
-                                A szállás éttermében vacsoráztunk, az ételek nagyon finomak voltak, jóllaktunk. Köszönjük a vendéglátást!"
-                            </div>
-                            <div>
-                                Gergely
-                            </div>
-                        </SlideItem>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideItem>
-                            <div>
-                                Nagyon családias, igazán vendéglátásra termett tulajdonosokkal találkozhattunk. Megtapasztalhattuk az igazi vendégszeretetet.
-                            </div>
-                            <div>
-                                Ferenc
-                            </div>
-                        </SlideItem>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideItem>
-                            <div>
-                                Kedvesek a vendéglátók, segítőkészek. Nagyon tiszta és igazán otthonos, remekül felszerelt szálláshely.
-                                Teljesen alkalmas az önellátásra, de ha akartam a vendéglő részüket is igénybe vehettem.
-                                Élelmiszerbolt a közvetlen szomszédságban. Jövőre is ide mennék!
-                            </div>
-                            <div>
-                                Márta
-                            </div>
-                        </SlideItem>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <SlideItem>
-                            <div>
-                                Mindenki nagyon kedves volt, segítőkész, látszott, hogy nekik fontos volt, hogy mi öten jól érezzük magunkat!
-                                A szállás tiszta volt, hűvös, csendes és nyugodt! Mindenben nagyon megfelelt, jól éreztük ott magunkat!
-                                Köszönjük szépen!
-                            </div>
-                            <div>
-                                Lívia
-                            </div>
-                        </SlideItem>
-                    </SwiperSlide>
+                    {
+                        testimonials.map(({ id, text, name }) => (
+                            <SwiperSlide key={id}>
+                                <SlideItem>
+                                    <div>
+                                        {text}
+                                    </div>
+                                    <p>
+                                        {name}
+                                    </p>
+                                </SlideItem>
+                            </SwiperSlide>
+                        ))
+                    }
+
                 </Swiper>
             </SwiperContainer>
 
@@ -87,89 +98,54 @@ export default function Testimonials() {
 const Container = styled.section`
     margin-top: 8rem;
     padding: 0 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 4rem;
-
-    @media(min-width: 768px) {
-        flex-direction: row;
-    }
 `;
 
-const Header = styled.div`
+const Title = styled.div`   
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 100%;
+    margin-bottom: 4rem;
 
-    @media(min-width: 768px) {
-        width: 50%;
-    }
-
-    p {
-        font-size: 2rem;
-        font-weight: 700;
-        color: var(--decor);
-        margin-bottom: 1rem;
+    div {
+        width: 25%;
+        border: 0.5px solid var(--light-night);
     }
 
     h2 {
-        font-size: 2rem;
-        color: var(--night);
-        text-align: center;
-        margin-bottom: 1rem;
-    }
-
-    div {
-        font-size: 1rem;
         color: var(--light-night);
-        text-align: center;
-        margin-bottom: 2rem;
-    }
-
-    button {
-        cursor: pointer;
-        background: transparent;
-        color: var(--decor);
+        border: 1px solid var(--light-night);
+        border-radius: 0.25rem;
         width: max-content;
         padding: 0.5rem 1rem;
-        border-radius: 0.25rem;
-        letter-spacing: 0.1rem;
-        font-size: 1rem;
     }
+`;
+
+const Info = styled.div`
+    font-size: 1rem;
+    color: var(--light-night);
+    text-align: center;
+    margin-bottom: 4rem;
+    letter-spacing: 0.125rem;
 `;
 
 const SwiperContainer = styled.div`
     width: 100%;
-
-    @media(min-width: 768px) {
-        width: 50%;
-    }
 `;
 
 const SlideItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
     background-color: var(--day);
-    padding: 1rem 1rem 2rem 1rem;
-    aspect-ratio: 1;
+    padding: 0 1rem 2rem 1rem;
 
-    @media(min-width: 1024px) {
-        margin-bottom: 4rem;
-    }
-
-    >div:nth-child(1) {
+    >div {
         font-size: 1rem;
         color: var(--light-night);
         margin-bottom: 1rem;
     }
 
-    >div:nth-child(2) {
+    >p {
         font-size: 1rem;
         color: var(--light-night);
         margin-bottom: 1rem;
+        letter-spacing: 0.125rem;
     }
 `;
