201792e5c8ff7bd2b745295c74742cd11062f602
xdevtomix <90643951+xdevtomix@users.noreply.github.com>
2023-07-31 20:59:55 +0200

    pricing - price changes

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -39,8 +39,8 @@ const seasons = [
                     { label: '3 fő', price: '40 000 Ft' },
                     { label: '4 fő', price: '45 000 Ft' },
                     { label: '5 fő', price: '50 000 Ft' },
-                    { label: '6 fő', price: '58 000 Ft' },
-                    { label: '7 fő', price: '62 000 Ft' },
+                    { label: '6 fő', price: '54 000 Ft' },
+                    { label: '7 fő', price: '58 000 Ft' },
                 ]
             }
         ],
@@ -52,38 +52,38 @@ const seasons = [
             {
                 name: 'Szoba 2',
                 prices: [
-                    { label: '1 fő', price: '14 000 Ft' },
-                    { label: '2 fő', price: '16 000 Ft' },
+                    { label: '1 fő', price: '13 000 Ft' },
+                    { label: '2 fő', price: '18 000 Ft' },
                 ]
             },
             {
                 name: 'Szoba 3',
                 prices: [
-                    { label: '1 fő', price: '14 000 Ft' },
-                    { label: '2 fő', price: '16 000 Ft' },
-                    { label: '3 fő', price: '20 000 Ft' },
+                    { label: '1 fő', price: '13 000 Ft' },
+                    { label: '2 fő', price: '18 000 Ft' },
+                    { label: '3 fő', price: '22 000 Ft' },
                 ]
             },
             {
                 name: 'Apartman 5',
                 prices: [
                     { label: '1 fő', price: '22 000 Ft' },
-                    { label: '2 fő', price: '25 000 Ft' },
-                    { label: '3 fő', price: '27 000 Ft' },
-                    { label: '4 fő', price: '36 000 Ft' },
-                    { label: '5 fő', price: '38 000 Ft' },
+                    { label: '2 fő', price: '27 000 Ft' },
+                    { label: '3 fő', price: '31 000 Ft' },
+                    { label: '4 fő', price: '35 000 Ft' },
+                    { label: '5 fő', price: '39 000 Ft' },
                 ]
             },
             {
                 name: 'Apartman 7',
                 prices: [
-                    { label: '1 fő', price: '25 000 Ft' },
-                    { label: '2 fő', price: '30 000 Ft' },
-                    { label: '3 fő', price: '35 000 Ft' },
-                    { label: '4 fő', price: '40 000 Ft' },
-                    { label: '5 fő', price: '45 000 Ft' },
-                    { label: '6 fő', price: '51 000 Ft' },
-                    { label: '7 fő', price: '54 000 Ft' },
+                    { label: '1 fő', price: '28 000 Ft' },
+                    { label: '2 fő', price: '33 000 Ft' },
+                    { label: '3 fő', price: '38 000 Ft' },
+                    { label: '4 fő', price: '43 000 Ft' },
+                    { label: '5 fő', price: '48 000 Ft' },
+                    { label: '6 fő', price: '52 000 Ft' },
+                    { label: '7 fő', price: '56 000 Ft' },
                 ]
             }
         ],
